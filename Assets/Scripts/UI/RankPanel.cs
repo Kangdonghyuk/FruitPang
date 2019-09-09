af8992e434e12cd107a3ca6048a0dd6864b5a07b
Liunx <kdh950812@gmail.com>
2019-09-09 13:56:28 +0900

    change image, error noshow rank in android

--- Assets/Scripts/UI/RankPanel.cs
+++ Assets/Scripts/UI/RankPanel.cs
@@ -6,33 +6,33 @@ using System;
 
 public class RankPanel : MonoBehaviour
 {
-    Text[] rankText = new Text[8];
-    int[] rankScore = new int[9];
+    Text[] rankText = new Text[5];
+    int[] rankScore = new int[6];
 
     void Awake() {
-        for(int index = 0; index < 8; index++) {
+        for(int index = 0; index < 5; index++) {
             rankText[index] = transform.GetChild(index).GetComponent<Text>();
             rankScore[index] = 0;
         }
-        rankScore[8] = 0;
+        rankScore[5] = 0;
     }
 
     void Start()
     {
         PlayerPrefs.GetInt("score1", 0);
 
-        for(int index = 0; index < 8; index++) {
+        for(int index = 0; index < 5; index++) {
             rankText[index].text = PlayerPrefs.GetInt("save_score_" + index.ToString(), 0).ToString();
             rankScore[index] = int.Parse(rankText[index].text);
         }
 
-        rankScore[8] = SystemMNG.I.rankScore;
+        rankScore[5] = SystemMNG.I.rankScore;
         SystemMNG.I.rankScore = 0;
 
         Array.Sort(rankScore);
         Array.Reverse(rankScore);
 
-        for(int index = 0; index < 8; index++) {
+        for(int index = 0; index < 5; index++) {
             rankText[index].text = rankScore[index].ToString();
             PlayerPrefs.SetInt("save_score_"+index.ToString(), rankScore[index]);
         }
