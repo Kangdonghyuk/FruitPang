1b61bfd634d870e0eb75e014878ebfb62bc63482
Liunx <kdh950812@gmail.com>
2019-09-25 16:25:12 +0900

    랭킹 수정 및 아이템 사운드 변경

--- Assets/Scripts/MNG/MenuMNG.cs
+++ Assets/Scripts/MNG/MenuMNG.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuMNG : MonoBehaviour
 {
+    public Button startBtn;
+    public Button rankBtn;
+
+    public GameObject rankPanel;
+
     void Awake() {
         if(SystemMNG.I == null)
             Instantiate(Resources.Load("Prefabs/System/SystemMNG"));
@@ -11,7 +17,7 @@ public class MenuMNG : MonoBehaviour
 
     void Start()
     {
-        
+        rankPanel.transform.localPosition = Vector3.right * 1500;
     }
 
     void Update()
@@ -21,5 +27,17 @@ public class MenuMNG : MonoBehaviour
 
     public void GameScene() {
         SystemMNG.I.LoadScene("GameScene");
+    }
+
+    public void ShowRankPanel() {
+        rankPanel.transform.localPosition = Vector3.zero;
+        startBtn.interactable = false;
+        rankBtn.interactable = false;
+    }
+
+    public void CloseRankPanel() {
+        rankPanel.transform.localPosition = Vector3.right * 1500;
+        startBtn.interactable = true;
+        rankBtn.interactable = true;
     }
 }
--- Assets/Scripts/UI/RankPanel.cs
+++ Assets/Scripts/UI/RankPanel.cs
@@ -6,31 +6,31 @@ using System;
 
 public class RankPanel : MonoBehaviour
 {
-    Text[] rankText = new Text[5];
-    int[] rankScore = new int[6];
+    Text[] rankText = new Text[8];
+    int[] rankScore = new int[9];
 
     void Awake() {
-        for(int index = 0; index < 5; index++) {
+        for(int index = 0; index < 8; index++) {
             rankText[index] = transform.GetChild(index+1).GetComponent<Text>();
             rankScore[index] = 0;
         }
-        rankScore[5] = 0;
+        rankScore[8] = 0;
     }
 
     void Start()
     {
-        for(int index = 0; index < 5; index++) {
+        for(int index = 0; index < 8; index++) {
             rankText[index].text = PlayerPrefs.GetInt("save_score_" + index.ToString(), 0).ToString();
             rankScore[index] = int.Parse(rankText[index].text);
         }
 
-        rankScore[5] = SystemMNG.I.rankScore;
+        rankScore[8] = SystemMNG.I.rankScore;
         SystemMNG.I.rankScore = 0;
 
         Array.Sort(rankScore);
         Array.Reverse(rankScore);
 
-        for(int index = 0; index < 5; index++) {
+        for(int index = 0; index < 8; index++) {
             rankText[index].text = rankScore[index].ToString();
             PlayerPrefs.SetInt("save_score_"+index.ToString(), rankScore[index]);
         }
